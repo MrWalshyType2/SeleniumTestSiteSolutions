07fbeae3602388be77b6647c2070955fe7f14e4a
MrWalshyType2 <mwalsh@qa.com>
2023-01-18 09:20:18 +0000

    Added hash routes to URLs

--- src/test/java/com/qa/selenium_4_sol_java/utils/ExerciseURL.java
+++ src/test/java/com/qa/selenium_4_sol_java/utils/ExerciseURL.java
@@ -1,10 +1,10 @@
 package com.qa.selenium_4_sol_java.utils;
 
 public enum ExerciseURL {
-	DRAG_N_DROP("http://localhost:3000/exercises/dragdrop"), 
-	FORMS("http://localhost:3000/exercises/forms"), 
-	TABLES("http://localhost:3000/exercises/tables"), 
-	WAITS("http://localhost:3000/exercises/waits");
+	DRAG_N_DROP("http://localhost:3000/#/exercises/dragdrop"), 
+	FORMS("http://localhost:3000/#/exercises/forms"), 
+	TABLES("http://localhost:3000/#/exercises/tables"), 
+	WAITS("http://localhost:3000/#/exercises/waits");
 	
 	private final String url;
 	
